62ee7d65da84e9961a4d3a531f5f6061ddc0eb7a
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2025-01-22 11:03:37 +0100

    a

--- c/sorting/main.c
+++ c/sorting/main.c
@@ -2,44 +2,64 @@
 #include <stdlib.h>
 #include <time.h>
 
-int includes(int ARR[16], int var){
+int includes(int ARR[6], int var){
 
-    for(int i = 0; i < 16; i++){
+    for(int i = 0; i < 6; i++){
         if(ARR[i] == var) return 1;    
     }
     return 0;
 }
-int generateRandomArray(){
+int correct(int arr[6]){
+    for(int i = 0; i<6; i++){
+        if(arr[i] != i) return 0;
+    }
 
-    int zeroIndex = rand()%16;
-    int pole[16] = {0};
+    return 1;
+}
+int logArray(int arr[6]){
 
-    for(int i = 0; i<16; i++){
+    printf("[%d", arr[0]);
+
+    for(int i = 1; i<6; i++){
+
+        printf(", %d", arr[i]);
+    }
+    printf("]\n");
+}
+int main(){
+    srand(time(NULL));
+
+    int zeroIndex = rand()%6;
+    int pole[6] = {0,0,0,0,0,0};
+
+    for(int i = 0; i<6; i++){
 
         if(i != zeroIndex){
          
             int num;
             do{
-                num = rand()%16;
+                num = rand()%6;
             }while(includes(pole, num));
 
             pole[i] = num;
         }
     }
 
-    return pole[16];
-}
-int main(){
-    srand(time(NULL));
+    while(!correct(pole)){
 
-    int arr[16] = {0};
+        for(int i = 0; i<6; i++){
 
-    arr[16] = generateRandomArray();
+            if(pole[i] > pole[i + 1]){
+                printf("\nkys\n");
 
-    for(int i = 0; i<16; i++){
-
-        printf("%d, ", arr[i]);
+                int kys = pole[i + 1];
+                pole[i + 1] = pole[i];
+                pole[i] = kys;
+            
+            }else break;
+        }
+        
+        logArray(pole);
     }
-
     return 0;
 }
