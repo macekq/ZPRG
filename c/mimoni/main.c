b5b60acc1cb7f8755d8bc20b2778cca9b75e2a23
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2025-04-24 12:31:05 +0200

    a

--- c/mimoni/main.c
+++ c/mimoni/main.c
@@ -2,7 +2,18 @@
 
 int main(){
 
-    
+    const char *oznameni = "vcera 23.4.2025 prileteli mimozemske bytosti do ghetta zvaneho 'privoz'\n\
+    ackoliv tyto bytosti nedali najevo znamky nasili nebo nebezpecnosti, obyvatele privozu nevahali a\
+    rozebrali mimozemske plavidla a prodali je do kovosrotu\nale nemusite se bat o svuj zivot(pokud nebydlite v privoze)\
+    vypada to ze mimozemske bytosti neopusti privoz nazivu";
+    const char *rozhovor = "rozhovor s panem Kobalt Jozko:\n\
+    ty gadze prileteli more a furt budu blbe cumet more ci som sebral par bratov more a my nemame problem\n\
+    sme je dobili za lidlem more a chabr sekl ty gadze o boty more";
+    const char *martanRozhovor = "dneska je to 36h od prichodu martanu a vetsina z nich je mrtva zmrzacena nebo se pohresuje\n\
+    blize se k teto situaci vyjadri pan X12#&-!!?:\n\
+    dablove jsou to doted rasista byl sem ne ovsem tmavou s pleti rad sem kdyz nemame doma\n
+    planetu navsevovat vasi nebudem kdyz oni zde jsou";    
+
 
     return 0;
 }
