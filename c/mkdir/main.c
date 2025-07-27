9006efb89d7b1966d0a78f2d83a52a4c54980a8d
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2025-07-27 19:26:40 +0200

    Update main.c

--- c/mkdir/main.c
+++ c/mkdir/main.c
@@ -5,7 +5,7 @@
 int main() {
     
     //vytvorit slozku
-    const char *path = "C:\\Users\\l.macura.st\\Documents\\ZPRG\\c\\mkdir"; //absolutni cesta
+    const char *path = "/home/macura/Documents/GitHub/ZPRG/c/mkdir"; //absolutni cesta
 
     if(_chdir(path) == 0){
         printf("true");
