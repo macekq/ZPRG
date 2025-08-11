45e37c94794fb3a4250a38de1558ccfe0036415b
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2025-08-11 14:36:37 +0200

    a

--- c/try.c
+++ c/try.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main(){
-
+/*
     int input;
     while(input!=27){
-        input = getch();
+        input = scanf();
         printf("%d\n", input);
         if(input == 77) printf("?huh?");
         if(input == 224) printf("?huh?");
     }
-
+*/
+    printf("skap");
     return 0;
 }
