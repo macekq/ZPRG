f3cc799b46949186451dcab9a6e756d5dd68bf72
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2026-02-02 10:34:12 +0100

    a

---
+++ java/02-02-26/src/vypisAdresare.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class vypisAdresare{
+    public static final String RESET = "\033[0m";
+    public static final String CYAN = "\033[0;36m";
+
+    public vypisAdresare(){
+
+        Scanner sc = new Scanner(System.in);
+        String directoryPath;
+
+        do{
+            System.out.println();
+            System.out.println("vlozte vas adresar:");
+            directoryPath = sc.nextLine();
+
+            obsahAdresare(directoryPath, 0);
+        }while(directoryPath != "");
+    }
+    public static void obsahAdresare(String dir, int distance){
+        File directory = new File(dir);
+
+        if (directory.exists() && directory.isDirectory()){
+
+            String[] files = directory.list();
+            for (String fileName : files){
+                File soubor = new File(dir + "\\" + fileName);
+
+                for(int i = 0; i<distance; i++)System.out.print(" - ");
+
+                if(!soubor.isFile()){
+                    System.out.print(CYAN + fileName + " ->" + soubor.length()/1024.0 + "KB\n" + RESET);
+                    obsahAdresare(soubor.getAbsolutePath(), distance+1);
+
+                }else System.out.print(fileName + " ->" + soubor.length()/1024.0 + "KB\n");
+            }
+
+        } else System.out.println("*");
+    }
+}
